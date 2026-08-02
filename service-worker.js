222f8f72c6d2f9dd39101f5a7e1744a92d35efb0
iSupport <isupport@MacBook-Pro-de-iSupport.local>
2026-08-02 07:37:32 -0500

    Corrección menú lápiz 6.36.6

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-offline-v6-20260802-entrega-6367";
+const VERSION = "egm-offline-v6-20260802-entrega-6368";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE = [
