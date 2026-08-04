fb64b079ae4b01dd198fb46d35594388543b40a4
iSupport <isupport@MacBook-Pro-de-iSupport.local>
2026-08-04 13:57:29 -0500

    Entrega 6.36.56 - Letra completa y bloqueo de cajas

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-offline-v6-20260804-entrega-63655";
+const VERSION = "egm-offline-v6-20260804-entrega-63656";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE = [
