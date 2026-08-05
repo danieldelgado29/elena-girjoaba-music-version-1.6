32651e47f239536e569983e3a20ffc20e017bdab
iSupport <isupport@MacBook-Pro-de-iSupport.local>
2026-08-05 12:29:07 -0500

    Entrega 6.36.68 - Interfaz cliente sin titileo

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-v6.36.72";
+const VERSION = "egm-v6.36.68";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE = [
