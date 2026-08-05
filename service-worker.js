fbf80d24ee7902050742312821681263498362e2
iSupport <isupport@MacBook-Pro-de-iSupport.local>
2026-08-05 12:38:35 -0500

    Entrega 6.36.67 - Show activo sincronizado

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-v6.36.73";
+const VERSION = "egm-v6.36.67";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE = [
