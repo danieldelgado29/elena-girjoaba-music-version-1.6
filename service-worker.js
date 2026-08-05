5b3803aeb779aa9a8df9da50870455b17050e1b1
iSupport <isupport@MacBook-Pro-de-iSupport.local>
2026-08-05 11:54:53 -0500

    Entrega 6.36.70 - Alinear y bloquear cajas de texto

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "egm-v6.36.68";
+const VERSION = "egm-v6.36.70";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE = [
